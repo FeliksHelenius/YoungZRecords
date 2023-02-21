9e2fc511d579b1bcd0fc27c41459ca4e1d533daa
FeliksHelenius <FeliksHelenius@gmail.com>
2023-02-21 13:33:52 +0100

    final

--- src/assets/icons.js
+++ src/assets/icons.js
@@ -4,13 +4,13 @@ let icons = {
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
-      stroke-width="1.5"
+      strokeWidth="1.5"
       stroke="currentColor"
-      class="menu-button-svg"
+      className="menu-button-svg"
     >
       <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
         d="M3.75 9h16.5m-16.5 6.75h16.5"
       />
     </svg>
