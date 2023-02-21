e3556c162e4a2915d775fd43f271ab1586e76986
FeliksHelenius <FeliksHelenius@gmail.com>
2023-02-21 14:00:47 +0100

    final commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <BrowserRouter basename="/YoungZRecords">
       <Routes>
-        <Route exact path="/YoungZRecords" element={<Landing />} />
+        <Route exact path="/" element={<Landing />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
     </BrowserRouter>
